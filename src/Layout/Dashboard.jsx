abb8b2b113a83a7ede117862368bf4d6e6c12f77
Khandaker Samin Yeasar <138380913+KhandakerSamin@users.noreply.github.com>
2023-12-22 03:15:15 +0600

    Modal done and dashboard info added

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,6 +1,9 @@
 import { Link, NavLink, Outlet } from 'react-router-dom';
+import { AuthContext } from '../Provider/AuthProvider';
+import { useContext } from 'react';
 
 const Dashboard = () => {
+    const { user } = useContext(AuthContext)
     return (
         <div className='flex'>
             <div className="w-[280px]  h-[100vh] sticky top-0  p-[35px] bg-[#1c2536] ">
@@ -19,9 +22,51 @@ const Dashboard = () => {
                     <NavLink className='btn btn-outline hover:bg-transparent hover:text-white hover:text-lg' to='/about'>About</NavLink>
                     <NavLink className='btn btn-outline hover:bg-transparent hover:text-white hover:text-lg' to='/contactUs'>Contact Us</NavLink>
                 </div>
+                <div className="sticky inset-x-0 bottom-0 border-t border-gray-100">
+                    <a href="#" className="flex items-center gap-2  px-4 pt-4 ">
+                        <img
+                            alt="Man"
+                            src="https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                            className="h-10 w-10 rounded-full object-cover"
+                        />
+
+                        <div>
+                            <p className="text-xs ">
+                                <strong className="block font-medium text-white">{user?.displayName}</strong>
+                            </p>
+                        </div>
+
+                    </a>
+                    <h1 className='text-xs px-3 pb-3'>{user?.email}</h1>
+                </div>
+                <div className="sticky inset-x-0 bottom-0 border-t border-gray-100 pt-4 p-2">
+                    <form action="/logout">
+                        <button
+                            type="submit"
+                            className="group relative flex w-full justify-center text-white items-center  rounded-lg px-2 py-1.5 text-sm gap-x-3 btn btn-outline hover:bg-gray-50 hover:text-gray-700"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5 opacity-75"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-width="2"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                                />
+                            </svg>
+
+                            Logout
+                        </button>
+                    </form>
+                </div>
             </div>
             <div className="flex-1">
-               
+
                 <Outlet></Outlet>
             </div>
         </div>
--- src/Pages/Dashboard/DashboardHome.jsx
+++ src/Pages/Dashboard/DashboardHome.jsx
@@ -1,16 +1,99 @@
 import { FaPlus } from "react-icons/fa6";
 
 const DashboardHome = () => {
+    const handleNewTask = e => {
+        e.preventDefault()
+        const form = e.target 
+        const title = form.title.value;
+        const description = form.description.value;
+        const priority = form.priority.value;
+        const deadline = form.deadline.value;
+        const type = "toDo"
+
+        const newTask = {
+            title,
+            description,
+            priority,
+            deadline,
+            type
+        }
+
+        console.log(newTask);
+
+    }
     return (
         <div>
             <nav className='h-20 flex justify-between items-center bg-[#1c2536]'>
                 <div>
                     <h1 className='text-white font-bold text-2xl mx-10 py-5 '>Platform Launch</h1>
                 </div>
-                <div className='bg-blue-700 rounded-2xl w-fit h-12 flex justify-center items-center mx-5'>
+                <button onClick={() => document.getElementById('my_modal_5').showModal()} className='bg-blue-700 rounded-2xl w-fit h-12 flex justify-center items-center mx-5'>
                     <h1 className='text-white font-bold mx-auto px-7 flex justify-between items-center gap-x-3'> <FaPlus className='text-xl' /> Add New Task</h1>
+                </button>
+                <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-2xl mb-6 text-white text-center">Add Your Task </h3>
+                        <form onSubmit={handleNewTask}>
+                            <div className="mb-2">
+                                <label className="sr-only" htmlFor="name">Title</label>
+                                <input
+                                    className="w-full rounded-lg border-gray-200 text-white p-3 text-sm"
+                                    placeholder="Title"
+                                    type="text"
+                                    id="title"
+                                    name="title"
+                                />
+                            </div>
+                            <div className="mb-1">
+                                <label className="sr-only" >Description</label>
 
-                </div>
+                                <textarea
+                                    className="w-full rounded-lg border-gray-200 text-white p-3 text-sm"
+                                    placeholder="Description"
+                                    rows="4"
+                                    id="description"
+                                    name="description"
+                                ></textarea>
+                            </div>
+                            <div className="flex justify-between items-center gap-x-2">
+                                <div className="w-1/2">
+                                    <select
+                                        name="priority"
+                                        id="priority"
+                                        className=" w-full rounded-lg border-gray-300  p-3 text-white sm:text-sm"
+                                    >
+                                        <option value="">Select Priority</option>
+                                        <option value="low">Low</option>
+                                        <option value="moderate">Moderate</option>
+                                        <option value="high">High</option>
+                                       
+                                    </select>
+                                </div>
+                                <div className="w-1/2">
+                                    <label className="sr-only" htmlFor="name">Deadline</label>
+                                    <input
+                                        className="w-full rounded-lg border-gray-200 text-white p-3 text-sm"
+                                        placeholder="Title"
+                                        type="date"
+                                        id="deadline"
+                                        name="deadline"
+                                    />
+                                </div>
+                            </div>
+                        <div className="modal-action flex justify-between items-center">
+                        <button onSubmit={handleNewTask} className="btn btn-outline hover:bg-transparent hover:text-white hover:font-bold px-6">Add Task</button>
+                            <form method="dialog">
+                                {/* if there is a button in form, it will close the modal */}
+
+
+                                <button className="btn btn-outline hover:bg-transparent hover:text-white hover:font-bold ">Close</button>
+
+                            </form>
+                        </div>
+                        </form>
+                        
+                    </div>
+                </dialog>
             </nav>
             <div className="grid grid-cols-4 m-5">
                 <div className="border-r-4 h-screen">
@@ -28,7 +111,7 @@ const DashboardHome = () => {
                         <h1 className="text-green-600 text-lg font-bold px-5 py-2  ">Completd (3)</h1>
                     </div>
                 </div>
-            
+
             </div>
         </div>
     );
